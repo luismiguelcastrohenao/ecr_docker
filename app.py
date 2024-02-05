71d13b912fc9e6b27b32110896284c88f305d83a
luis miguel castro <luismiguelcastrohenao@gmail.com>
2024-02-05 09:58:49 -0500

    feat: Update Dockerfile - Updated python image in the Dockerfile - Updated application response

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Karen la dormilona que no me presta atención</h1><br>' \
+    return '<h1>Vamos con toda</h1><br>' \
            '<h2>Have a Good day</h2>'
    
 if __name__ == "__main__":
